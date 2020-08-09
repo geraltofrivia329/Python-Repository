48d5cc588059e9348f7954d8936b27fe8bcecb87
geraltofrivia69 <charneuk@gmail.com>
2020-08-09 18:47:48 -0500

    Add files via upload

---
+++ StockTicker2020v6.py
@@ -0,0 +1,77 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+from dash.dependencies import Input, Output, State
+import plotly.graph_objs as go
+from datetime import datetime
+from iexfinance.stocks import get_historical_data
+import os
+
+
+app = dash.Dash()
+
+nsdq = pd.read_csv('C:/Users/Chuck/Documents/Python Scripts/NASDAQcompanylist.csv')
+nsdq.set_index('Symbol',inplace=True)
+options=[]
+
+for tic in nsdq.index:
+    #label:'users sees','value':'script sees'
+    mydict={}
+    mydict['label']=str(nsdq.loc[tic]['Name']) + ' ' +tic
+    mydict['value'] = tic
+    options.append(mydict)
+
+app.layout = html.Div([
+        html.H1("Stock Ticker Dashboard"),
+        html.Div([html.H3("Enter a stock symbol:",style={'paddingRight':'30px'}),
+            dcc.Dropdown(id='my_stock_ticker',
+                options = options,
+                value=['TSLA'],
+                multi = True
+            )],style={'display':'inline-block','verticalAlign':'top'}),
+        html.Button(id='submit-button',
+                n_clicks=0,
+                children='Submit Here',
+                style={'fontSize':24}),
+
+        html.Div([html.H3('Select a start and end date:'),
+            dcc.DatePickerRange(
+                id="my-date-picker-range",
+                min_date_allowed=datetime(2020,3,1),
+                max_date_allowed=datetime(2020,6,24),
+                initial_visible_month=datetime(2020,3,1),
+                start_date=datetime(2020,3,1),
+                end_date=datetime(2020,6,24)
+            ),
+        ],style={'display':'inline-block'}),
+
+            dcc.Graph(id='my_graph',
+            figure={'data':[
+            {'x':[1,2,3], 'y':[4,5,6]}
+            ], 'layout':{'title':'Default TItle'}})
+
+
+])
+
+@app.callback(Output('my_graph','figure'),
+            [Input('submit-button', 'n_clicks')],
+                [State('my_stock_ticker','value'),
+                State('my-date-picker-range','start_date'),
+                State('my-date-picker-range','end_date')])
+def update_graph(n_clicks,StockData,start_date,end_date):
+    start = datetime.strptime(start_date[:10],'%Y-%m-%d')
+    end = datetime.strptime(end_date[:10],'%Y-%m-%d')
+    os.environ["IEX_TOKEN"] = "pk_a3bffefd4f784874b9f9cff898578a74"
+    traces = []
+    for tic in my_stock_ticker:
+        df = get_historical_data(tic,start=start,end=end,close_only=True, output_format='pandas')
+        traces.append({'x':df.index,'y':df['close'],'name':tic})
+    fig = {'data':traces,
+    'layout':{'title':StockData}
+    }
+    return fig
+
+
+if __name__=='__main__':
+    app.run_server()
